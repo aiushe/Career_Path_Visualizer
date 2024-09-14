121998f2040a3297907b67595fd63728e05ffd25
AiusheM <55148666+AiusheM@users.noreply.github.com>
2024-09-13 17:24:19 -0700

    upload resume working

--- server/server.js
+++ server/server.js
@@ -12,3 +12,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+//resume data upload
+app.post('/upload-resume', (req, res) => {
+  const resumeData = req.body;
+
+  console.log("Received POST request at /upload-resume");
+
+  console.log("Resume Data:", resumeData);
+
+  res.status(200).send('Resume uploaded successfully');
+});
+
